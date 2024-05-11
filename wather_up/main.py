97f9919bf99324cc93954e72cb1a0be653520594
Andriihoz <140732360+Andriihoz@users.noreply.github.com>
2024-05-11 12:32:47 +0300

    Add files via upload

--- wather_up/main.py
+++ wather_up/main.py
@@ -1,9 +1,17 @@
+from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDscreen
+
+class MainScreen(MDScreen):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
 
 
-class MainApp(MDApp):
+class WatherApp(MDApp):
     def build(self):
+        Builder.load_file('style.kv')
+        self.screen = MainScreen('main_screen')
         return MDLabel(text="Hello, World", halign="center")
 
 
